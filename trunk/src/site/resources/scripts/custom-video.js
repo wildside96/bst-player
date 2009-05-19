9dcc6a33955b46d5a64c1ec86e98196e5ef9531d
sbraheem <sbraheem@365b5a48-e796-11dd-bd7b-b12b6590754f>
2009-05-19 08:18:43 +0000

    Parent POM updated, project site-docs added

---
+++ trunk/src/site/resources/scripts/custom-video.js
@@ -0,0 +1,54 @@
+/* 
+ *  Copyright 2009 Sikirulai Braheem <sbraheem at bramosystems.com>.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+var bst_player_1 = {
+    divId : "ex_1",
+    skin : "FlatVideoPlayer",
+    plugin : "WinMediaPlayer",
+    url : "http://bst-player.googlecode.com/svn/tags/showcase/media/teaching-of-islam.wmv",
+    autoplay : false,
+    height : "300px",
+    width : "70%"
+};
+var bst_player_2 = {
+    divId : "ex_2",
+    skin : "FlatVideoPlayer",
+    plugin : "QuickTimePlayer",
+    url : "http://movies.apple.com/movies/paramount/star_trek/startrek-tlr3_h.640.mov",
+    autoplay : false,
+    height : "350px",
+    width : "70%"
+};
+var bst_player_3 = {
+    divId : "ex_3",
+    skin : "FlatVideoPlayer",
+    plugin : "FlashMP3Player",
+    url : "http://vixy.net/flv/vixy_net_podcaster_demo_director.flv",
+    autoplay : false,
+    height : "350px",
+    width : "70%"
+};
+var bst_player_4 = {
+    divId : "ex_4",
+    skin : "FlatVideoPlayer",
+    plugin : "Auto",
+    url : "gwt-demo/media/sample.mov",
+    autoplay : false,
+    height : "300px",
+    width : "70%",
+    showLogger : true
+};
