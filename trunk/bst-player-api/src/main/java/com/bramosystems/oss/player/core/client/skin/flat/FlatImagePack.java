eb82154d12ae1924a15c1403eeba12b929175901
berger.max <berger.max@365b5a48-e796-11dd-bd7b-b12b6590754f>
2010-04-14 10:36:54 +0000

    Updated Skin to gwt 2.0; refactored default skin into flat/ directory

---
+++ trunk/bst-player-api/src/main/java/com/bramosystems/oss/player/core/client/skin/flat/FlatImagePack.java
@@ -0,0 +1,42 @@
+package com.bramosystems.oss.player.core.client.skin.flat;
+
+import com.bramosystems.oss.player.core.client.skin.ImagePack;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+/**
+ * Simple Image Pack with flat blue images.
+ */
+public interface FlatImagePack extends ImagePack, ClientBundle {
+    public ImageResource pause();
+
+    public ImageResource pauseHover();
+
+    public ImageResource play();
+
+    public ImageResource playHover();
+
+    public ImageResource playDisabled();
+
+    public ImageResource stop();
+
+    public ImageResource stopDisabled();
+
+    public ImageResource stopHover();
+
+    public ImageResource spk();
+
+    public ImageResource spkHover();
+
+    public ImageResource prev();
+
+    public ImageResource prevDisabled();
+
+    public ImageResource prevHover();
+
+    public ImageResource next();
+
+    public ImageResource nextDisabled();
+
+    public ImageResource nextHover();
+}
